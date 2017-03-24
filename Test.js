8b716420f82a4a8c7cd6caa80a394b8d2bfb446f
komo91 <lures.gx.91@gmail.com>
2017-03-24 14:58:25 +0900

    add site script

--- Test.js
+++ Test.js
@@ -1,40 +1,56 @@
-console.log('test');
-
-function a() {
-  if(navigator.geolocation) {
-    LocationTest();
-  }
-  console.log('test2');
+if(navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function( position )
+    {
+      var data = position.coords;
+      
+      var lat = data.latitude;
+      var lng = data.longitude;
+      var alt = data.altitude;
+      var accLatlng = data.accuracy;
+      var accAlt = data.altitudeAccuracy;
+      var heading = data.heading;
+      var speed = data.speed;
+      
+      alert("現在位置は¥n[" + lat + "," + lng + "]¥nです");
+      
+      document.getElementById('result').inerHTML = '<dl><dt>緯度</dt><dd>' + lat + '</dd><dt>経度</dt><dd>' + lng + '</dd><dt>高度</dt><dd>' + alt + '</dd><dt>緯度、経度の精度</dt><dd>' + accLatlng + '</dd><dt>高度の精度</dt><dd>' + accAlt + '</dd><dt>方角</dt><dd>' + heading + '</dd><dt>速度</dt><dd>' + speed + '</dd><dl>';
+      
+    },
+    
+    function(error)
+    {
+      var errorInfo = [
+        "原因不明のエラー",
+        "位置情報取得許可されない",
+        "電波状況で位置情報取得できず",
+        "タイムアウト"
+      ];
+      
+      var errorNo = error.code;
+      
+      var errorMessage = "[エラー番号:" + errorNo + "]¥n" + errorInfo[errorNo];
+      
+      alert(errorMessage);
+      
+      document.getElementById("result").innerHTML = errorMessage;
+      
+    },
+    
+    {
+      "enableHighAccuracy": false;
+      "timeout": 8000,
+      "maximumAge": 2000,
+    }
+  );
 }
-
-funtion LocationTest() {
-  navigator.geolocation.getCurrentPosition(succesFunc,errorFunc,optionObj);
+else 
+{
+  var errorMessage = "御使いの端末は、GeoLocationAPIに対応していません"
   
+  alert(errorMessage);
+  
+  document.getElementById('result').innerHTML = errorMessage;
 }
-
-function successFunc(position) {
-  var data = position.coords;
-  
-  alert("緯度:" + data.latitude + "経度:" + data.longitude); //緯度,経度
-  
-  document.getElementById('result').innerHTML = '<dl><dt>緯度</dt></dd>' + data.latitude + '</dd><dt>経度</dd></dl>';
-  
 }
-
-function errorFunc(error) {
-  //
-  var errorMessage = {
-    0: "原因不明",
-    1: "位置情報取得不許可",
-    2: "位置情報取得電波状況",
-    3: "位置情報取得タイムアウト",
-  };
-  
-  alert(errorMessage[error.code]);
 }
-
-var optionObj = {
-  "enableHighAccuracy": false,
-  "timeout": 8000,
-  "maximuAge": 5000,
-};
