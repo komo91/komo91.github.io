2fb3ab440d14d21ab8252e0760957abf1961f6ab
komo91 <lures.gx.91@gmail.com>
2017-04-28 15:25:16 +0900

    4/28 circle範囲テスト

--- Test2.js
+++ Test2.js
@@ -149,7 +149,7 @@ var watchId = navigator.geolocation.watchPosition( successFunc, errorFunc, optio
 
 function decision() { //目的地判定
   for(var j = 1; j < CheckData.length; j++) {
-    var distance = google.maps.geometry.spherical.computeDisranceBetween(marker[j].position,myPosition);
+    var distance = google.maps.geometry.spherical.computeDistanceBetween(myPosition,marker[j].position);
     if(CirclePoint[j].radius　<　distance) {
       alert(CheckData[j]['message']);
     }
