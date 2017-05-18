c6886658c30089cfa2c12c4a20af2bc611b5d365
komo91 <lures.gx.91@gmail.com>
2017-05-18 17:32:38 +0900

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -66,10 +66,16 @@ var CheckData = [ //位置情報配列
     radius: 20,
     message: "権現茶屋ですよ"
   },{
-    name: '高尾山薬王院',
-    lat: 35.625866,
-    lng: 139.250263,
+    name: 'もみじや',
+    lat: 35.626204,
+    lng: 139.250899,
     radius: 20,
+    message: "もみじやですよ"
+  },{
+    name: '高尾山薬王院',
+    lat: 35.625807,
+    lng: 139.249408,
+    radius: 40,
     message: "薬王院ですよ"
   }, {
     name: '山頂前トイレ',
