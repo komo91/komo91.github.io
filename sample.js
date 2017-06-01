0ba57ac5dd817fabe90b841bcf53efe0a1ba9349
nishitalab <owner@nishitalab-no-MacBook-Pro.local>
2017-06-01 15:25:57 +0900

    XPath 検証11

--- sample.js
+++ sample.js
@@ -2,7 +2,7 @@ var p = document.getElementById('sample');
 
 $x('/html/body/div[0]/');
 
-var result = document.evaluate('/html,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null);
+var result = document.evaluate('/html',document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null);
 
 var node;
 while(node=result.iterateNext()) {
