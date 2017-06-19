e19cb5ce43538788aae844cc1ad4e6b415b89cfe
komo91 <lures.gx.91@gmail.com>
2017-06-19 17:16:25 +0900

    Update Test2.js

--- js/Test2.js
+++ js/Test2.js
@@ -167,8 +167,6 @@ if(navigator.geolocation) {
         position: myPosition
       });
       
-      syncerWatchPosition.map.setCenter(myPosition);  //地図中心変更
-      
     } else {
       syncerWatchPosition.map.setCenter(myPosition);  //地図中心変更
     } 
