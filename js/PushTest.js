e75d3c2c2f7ef3a2e0426b5f1eccbd5657c25c6b
nishitalab <owner@nishitalab-no-MacBook-Pro.local>
2017-06-20 13:32:11 +0900

    PushTest 6

--- js/PushTest.js
+++ js/PushTest.js
@@ -3,6 +3,8 @@ Push.create("Hello wolrd!",{
 	body: "How's it hangin'?",
 	icon: 'img/mountain_icon.png',
 	timeout: 10000,
+	vibrate: [100,100],
+	link: "https://komo91.github.io/PushTest.html"
 	onClick: function (){
 		window.focus();
 		this.close();
