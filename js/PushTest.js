ef10091615a6148738f3fbe073643b482a026805
nishitalab <owner@nishitalab-no-MacBook-Pro.local>
2017-06-20 13:19:38 +0900

    PushTest 5

--- js/PushTest.js
+++ js/PushTest.js
@@ -1,9 +1,10 @@
 
 Push.create("Hello wolrd!",{
 	body: "How's it hangin'?",
+	icon: 'img/mountain_icon.png',
 	timeout: 10000,
 	onClick: function (){
-		windows.focus();
+		window.focus();
 		this.close();
 	}
 });
