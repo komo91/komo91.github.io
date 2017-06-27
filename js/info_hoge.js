192843f12534353679469f9f91e5891284ab5cd3
nishitalab <owner@nishitalab-no-MacBook-Pro.local>
2017-06-27 16:49:15 +0900

    ajax Test19

--- js/info_hoge.js
+++ js/info_hoge.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
 			url: 'https://weather.yahoo.co.jp/weather/jp/13/4410/13201.html',
 			dataType: 'html',
 		}).done(function(data) {
-			console.log(data);
+			console.log(data.responseText);
 			$('#hoge').html(data.responseText);
 			$table = $('table',$('#hoge'));
 			$('#Sample1').html($table.parent().prev().html());
