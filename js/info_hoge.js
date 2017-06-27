904b592f4f7926a6be38abe44cc293ef601043ba
nishitalab <owner@nishitalab-no-MacBook-Pro.local>
2017-06-27 11:40:38 +0900

    ajax test1

---
+++ js/info_hoge.js
@@ -0,0 +1,14 @@
+var url ="https://www.yahoo.co.jp/"
+
+function readHtml(url) {
+	$.ajax({
+		type: 'GET',
+		url: url,
+		success: function(data) {
+			$('#content').append(data.responseText);
+		},
+		error: function(xhr,status,err) {
+			alert('HTML読み出しで問題がありました:' + url);
+		}
+	});
+}
