aa6dc0f757891aeba5eeca89f78491a6a2daef85
nishitalab <owner@nishitalab-no-MacBook-Pro.local>
2017-06-28 18:26:59 +0900

    http テスト

--- js/info_hoge.js
+++ js/info_hoge.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
 		console.log("hoge");
 		$.ajax({
 			type: 'GET',
-			url: 'LocationTest2.html',
+			url: 'http://takaovc599.ec-net.jp/',
 			dataType: 'html',
 		}).done(function(data) {
 			console.log(data);
