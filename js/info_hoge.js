a04d6d6f6d9fa2023ea125db93f904f465b053d7
nishitalab <owner@nishitalab-no-MacBook-Pro.local>
2017-06-29 12:26:17 +0900

    title only view edit5

--- js/info_hoge.js
+++ js/info_hoge.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
 			console.log(data);
 			//$('#hoge').html(data);
 			$table = $('table',$('#hoge'));
-			$title = $('title',$('#hoge').data());
+			$title = $('title'.data());
 			$('#Sample1').html($title.html());
 			$('#Sample2').html($table.parent().html());
 		});
