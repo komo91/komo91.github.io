3b4a5297492f9c5a6bc7adcccb98a9a07520feff
nishitalab <owner@nishitalab-no-MacBook-Pro.local>
2017-06-29 11:31:58 +0900

    ajax 取得テスト

--- js/info_hoge.js
+++ js/info_hoge.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
 		console.log("hoge");
 		$.ajax({
 			type: 'GET',
-			url: 'Accleration.html',
+			url: 'Acceleration.html',
 			dataType: 'html',
 		}).done(function(data) {
 			console.log(data);
