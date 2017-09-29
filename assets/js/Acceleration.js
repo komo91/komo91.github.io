807a4eb43476add3d2350459cd7ef6ff4658b7ec
komo91 <lures.gx.91@gmail.com>
2017-09-29 15:12:57 +0900

    acc warning decision

--- assets/js/Acceleration.js
+++ assets/js/Acceleration.js
@@ -1,3 +1,5 @@
+var view = true;
+
 window.addEventListener("devicemotion",function(e){
    //加速度
    var acc = e.acceleration;
@@ -20,17 +22,12 @@ window.addEventListener("devicemotion",function(e){
    var r_c = obj2NumberFix(rota_r.gamma,2);
    */
 
-   function obj2NumberFix(obj,fix_deg) {
-      return Number(obj).toFixed(fix_deg);
+   if(view==true) {
+     warning_view();
+     view = false;
    }
 
-   //歩きスマホしてる時のみ表示させる
-   if(x>=0.5 || y>=1.3 || z>=2.0) {
-     //warning_view();
-     //alert('歩きスマホダメゼッタイ！');  //レイヤ透明度を低く調整したい
-   } else {
-     //document.getElementById('result').style.visibility = "hidden";
-   }
+   acceleration_decision(x,y,z);
 
    var mes = document.getElementById('result').innerHTML = "送信中...";
 
@@ -40,45 +37,59 @@ window.addEventListener("devicemotion",function(e){
    print_3('acc-x',x,'acc-y',y,'acc-z',z);
    print_3('acc-gx',gx,'acc-gy',gy,'acc-gz',gz);
    //print_3('rx',r_a,'ry',r_b,'rz',r_c);
-
-    function print_3(id1,value1,id2,value2,id3,value3) {
-      print_1(id1,value1);
-      print_1(id2,value2);
-      print_1(id3,value3);
-   }
-   function print_1(id,value) {
-      var id_obj = document.getElementById(id);
-      id_obj.innerHTML = value;
-   }
-
-   //歩きスマホ警告
-   function warning_view() {
-     var tar = document.getElementById('sub');
-
-     var a_height = Math.max(document.body.clientHeight,document.body.scrollHeight);
-     var b_height = Math.max(document.documentElement.scroollheight,document.documentElement.clientHeight);
-     var max_height = Math.max(a_height,b_height);
-     tar.style.height - max_height + 'px';
-
-     var a_width = Math.max(document.body.clientWidth,document.body.scrollWidth);
-     var b_width = Math.max(document.documentElement.scrollWidth,document.documentElement.clientWidth);
-     var max_width = Math.max(a_width,b_width);
-     tar.style.width = max_width + 'px';
-   }
-
-   function walk_log(x,y,z) {
-     var script = document.createElement('script');
-     var base = 'https://script.google.com/macros/s/AKfycbzZ3mZG_xRCN9OaOYIViGn9PQMyHvbgS6PdAtENGAEWVRzZ2LE/exec';
-     script.src = base + '?callback=receiveJson&x=' + x + '&y=' + y + '&z=' + z;
-     document.body.appendChild(script);
-   }
-
-   function receiveJson(json) {
-     mes = "送信完了!";
-   }
-
-   function sleep(wait_time) {
-     var start = new Date();
-     while(new Date() - start < wait_time);
-   }
 });
+
+function obj2NumberFix(obj,fix_deg) {
+   return Number(obj).toFixed(fix_deg);
+}
+
+function print_3(id1,value1,id2,value2,id3,value3) {
+  print_1(id1,value1);
+  print_1(id2,value2);
+  print_1(id3,value3);
+}
+function print_1(id,value) {
+  var id_obj = document.getElementById(id);
+  id_obj.innerHTML = value;
+}
+
+//歩きスマホ警告
+function warning_view() {
+  var tar = document.getElementById('sub');
+
+  var a_height = Math.max(document.body.clientHeight,document.body.scrollHeight);
+  var b_height = Math.max(document.documentElement.scroollheight,document.documentElement.clientHeight);
+  var max_height = Math.max(a_height,b_height);
+  tar.style.height - max_height + 'px';
+
+  var a_width = Math.max(document.body.clientWidth,document.body.scrollWidth);
+  var b_width = Math.max(document.documentElement.scrollWidth,document.documentElement.clientWidth);
+  var max_width = Math.max(a_width,b_width);
+  tar.style.width = max_width + 'px';
+}
+
+function walk_log(x,y,z) {
+  var script = document.createElement('script');
+  var base = 'https://script.google.com/macros/s/AKfycbzZ3mZG_xRCN9OaOYIViGn9PQMyHvbgS6PdAtENGAEWVRzZ2LE/exec';
+  script.src = base + '?callback=receiveJson&x=' + x + '&y=' + y + '&z=' + z;
+  document.body.appendChild(script);
+}
+
+function receiveJson(json) {
+  mes = "送信完了!";
+}
+
+function sleep(wait_time) {
+  var start = new Date();
+  while(new Date() - start < wait_time);
+}
+
+function acceleration_decision(x,y,z) {
+  //歩きスマホしてる時のみ表示させる
+  if(x>=0.5 || y>=1.3 || z>=2.0) {
+    document.getElementById('sub').style.visibility = "visible";
+    //alert('歩きスマホダメゼッタイ！');  //レイヤ透明度を低く調整したい
+  } else {
+    document.getElementById('result').style.visibility = "hidden";
+  }
+}
