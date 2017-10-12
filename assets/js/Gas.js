d73b5e1447521a70433a7e631f1b1eb7f68dbc13
komo91 <lures.gx.91@gmail.com>
2017-10-12 15:27:47 +0900

    screenshot view2

--- assets/js/Gas.js
+++ assets/js/Gas.js
@@ -347,7 +347,7 @@ function onDeviceMotion(e) {
     }
   }
   console.log(step + "歩");
-  document.getElementById('hoge').innerHTML = step + "歩";
+  //document.getElementById('hoge').innerHTML = step + "歩";
 
   //歩行状態ではないかつ歩行停止1秒後
   if(!isStep)  document.getElementById('sub').style.visibility = "hidden";
