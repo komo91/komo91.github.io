0a0dd04aca1f759cc7c0c09e15bbc119df5add6c
komo91 <lures.gx.91@gmail.com>
2017-10-26 17:52:45 +0900

    push test6

--- assets/js/Gas.js
+++ assets/js/Gas.js
@@ -220,6 +220,7 @@ function receiveJson(json) {
       document.getElementById('gas_url').appendChild(a);
       Speech(json.response[0]);
       //$(document).ready(PushTest(i,json.response[1]));
+    }
   }
   if(!json.response){
     document.getElementById('gas_result').innerHTML = json.error;
