82c7b18f859e22839e7518655ea056313f3a77b6
komo91 <lures.gx.91@gmail.com>
2017-11-02 18:27:25 +0900

    volue 0.1

--- assets/js/Gas.js
+++ assets/js/Gas.js
@@ -352,7 +352,7 @@ function Audio() {
     var gainNode = audioCtx.createGain();
     sourceNode.buffer = buffer;                     //再生サウンド設定
     sourceNode.connect(audioCtx.destination);       //connectにAudioNodeの値
-    gainNode.gain.value = 0.5;
+    gainNode.gain.value = 0.1;
     sourceNode.start(0);                            //0秒後に再生
   };
 
